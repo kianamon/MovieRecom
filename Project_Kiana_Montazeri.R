660f0733481cc2540c336337e9d563291e795e33
Kiana Montazeri <k.montazeri91@gmail.com>
2018-05-08 17:52:58 -0400

    Add files via upload

---
+++ Project_Kiana_Montazeri.R
@@ -0,0 +1,75 @@
+#Set the working directory:
+setwd("/Users/Kianamon/R/project")
+#####################################################################################
+#libraries in use:
+library(knitr)
+library(httr)
+library(readr)
+library(dplyr)
+library(tidyr)
+library(XML)
+library(ggplot2)
+library(stringr)
+library(lubridate)
+library(grid)
+library(caret)
+#####################################################################################
+#check for missing packages and install them:
+list.of.packages <- c("knitr", "httr", "readr", "dplyr", "tidyr", "XML",
+                      "ggplot2", "stringr", "lubridate", "grid", "caret")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+#####################################################################################
+#downloading the two main data sets:
+#All the movies from IMDB website:
+GET("https://raw.githubusercontent.com/kianamon/MovieRecom/master/movies.csv", 
+    write_disk("movies.csv", overwrite = TRUE))
+df_movies <- read_csv("movies.csv")
+#All the movies that I have watched:
+GET("https://raw.githubusercontent.com/kianamon/MovieRecom/master/kiana_watchlist.csv", 
+    write_disk("kianamovies.csv", overwrite = TRUE))
+df_kianamovies <- read_csv("kianamovies.csv")
+#####################################################################################
+#renaming the columns for convenience:
+names(df_kianamovies)[names(df_kianamovies) == 'IMDb Rating'] <- 'Score'
+names(df_kianamovies)[names(df_kianamovies) == 'Runtime (mins)'] <- 'Duration'
+names(df_kianamovies)[names(df_kianamovies) == 'Your Rating'] <- 'KianaRating'
+names(df_kianamovies)[names(df_kianamovies) == 'Title Type'] <- 'Type'
+names(df_kianamovies)[names(df_kianamovies) == 'Num Votes'] <- 'Num_Votes'
+
+names(df_movies)[names(df_movies) == 'movie_title'] <- 'Title'
+names(df_movies)[names(df_movies) == 'movie_imdb_link'] <- 'URL'
+names(df_movies)[names(df_movies) == 'num_voted_users'] <- 'Num_Votes'
+names(df_movies)[names(df_movies) == 'movie_title'] <- 'Title'
+names(df_movies)[names(df_movies) == 'director_name'] <- 'Directors'
+names(df_movies)[names(df_movies) == 'imdb_score'] <- 'Score'
+names(df_movies)[names(df_movies) == 'duration'] <- 'Duration'
+names(df_movies)[names(df_movies) == 'genres'] <- 'Genres'
+#####################################################################################
+#we have a big data set, we train our model for the kiana data set and apply the results 
+#to the big data set and calculate the likability of the movie for the big data set.
+#The output of the system is the Likability of each movie.
+movies1 <- df_movies %>%
+  select(Title, URL, Score, Duration, Genres, Directors, Num_Votes)
+kiana <- df_kianamovies %>%
+  select(Title, URL, Score, Duration, Genres, Directors, Num_Votes, KianaRating)
+#examining user's watched movies to see a pattern:
+#I am definig a factor as Likability to calculate a score for each movie,
+#Likability goes from 0 to 100 which is the probabilty of me liking a movie
+df_kiana <- kiana %>%
+  mutate(Likability = KianaRating*10)
+#####################################################################################
+#from now on, we are only working with df_kiana data set:
+#first we save the data set to a csv file:
+#saving the data frame in a csv file
+write.table(df_kiana,  
+            paste0("/Users/Kianamon/R/project/kiana.csv"), row.names=F)
+write.table(movies,  
+            paste0("/Users/Kianamon/R/project/bigsetofmovies.csv"), row.names=F)
+#####################################################################################
+#now we should manipulate the data to do the analysis:
+df_kiana1 <- as.data.frame(df_kiana) %>% 
+  separate(Genres, into = paste("Genres", 1:4, sep = ""), sep = ",")
+head(df_kiana1)
+#I hace decided to solve the problem only based on the 4 primary genres that are provided.
+#####################################################################################
