22c20dc809f395a0f23f26202120766cb9f22bad
Kiana Montazeri <k.montazeri91@gmail.com>
2018-05-21 16:44:41 -0400

    Update Project_Kiana_Montazeri.R

--- Project_Kiana_Montazeri.R
+++ Project_Kiana_Montazeri.R
@@ -13,6 +13,10 @@ library(stringr)
 library(lubridate)
 library(grid)
 library(caret)
+library(ranger)
+library(rpart)
+library()
+library()
 #####################################################################################
 #check for missing packages and install them:
 list.of.packages <- c("knitr", "httr", "readr", "dplyr", "tidyr", "XML",
@@ -88,15 +92,78 @@ df_kianatrain$Duration[is.na(df_kianatrain$Duration)] <- 100
 colSums(is.na(df_kianatrain))
 #I hace decided to solve the problem only based on the 4 primary genres that are provided.
 #####################################################################################
+#caret
+set.seed(1234)
+myControl1 <- trainControl(method = "cv", number = 5)
+grid <- expand.grid(alpha = 1, lambda = 0)
+modcaret <- train(Likability ~ .-URL-Title, df_kianatrain, method = "glmnet",
+family = "multinomial", trControl = myControl1,
+tuneGrid = grid, metric = "Accuracy")
+modcaret$results$Accuracy
+
+#caret2
 set.seed(1234)
 trControl <- trainControl(method = "cv", number = 5)
 grid <- expand.grid(alpha = 1, lambda = seq(0, 1.5, length = 201))
-mod <- train(Likability ~ .-URL-Title, df_kianatrain, method = "glmnet", tuneGrid = grid, 
+modcaret2 <- train(Likability ~ .-URL-Title, df_kianatrain, method = "glmnet", tuneGrid = grid, 
              trControl = trControl, metric = "RMSE", preProcess = c("center", "scale"))
 par(mar = c(4, 4, 0, 0))
-plot(mod)
-Beta <- coef(mod$finalModel, 0.75)
-R2 <- mod$results$Rsquared[which(grid$lambda == 0.75)]
+plot(modcaret2)
+Beta <- coef(modcaret2$finalModel, 0.75)
+R2 <- modcaret2$results$Rsquared[which(grid$lambda == 0.75)]
 1 - (1 - R2) * (nrow(df_kianatrain) - 1)/(nrow(df_kianatrain) - sum(Beta != 0) - 1)
+
+
+#glmnet:
+library(glmnet)
+x <- as.matrix(select(df_kianatrain, -URL-Title))
+y <- df_kianatrain$Likability
+mod_glmnet <- glmnet(x, y, family = "multinomial", lambda = 0)
+pred_glmnet <- predict(mod_glmnet, x, type = "response")
+head(pred_glmnet[, , 1])
+predglmnet <- predict(mod_glmnet, x, type = "class")
+table(predglmnet)
+confusionMatrix(predglmnet, df_kianatrain$Likability)
+
+
+
+#tree based
+myControl2 <- trainControl(method = "cv", number = 5, classProbs = TRUE)
+grid <- expand.grid(cp = c(0, 0.1, 0.3, 0.6))
+modrpart <- train(Likability ~ .-URL-Title, df_kianatrain, method = "rpart", trControl = myControl2, 
+    tuneGrid = grid, metric = "Accuracy")
+modrpart$results
+
+#grid search
+set.seed(1234)
+k = 5
+folds <- createFolds(df_kianatrain$Likability, k, list = FALSE)
+mygrid <- expand.grid(minsplit = c(1, 2, 5, 10, 20), maxdepth = c(1, 2, 3, 4, 5), 
+cp = c(0, 0.01, 0.1, 0.3, 0.5, 1))
+mod_accuracy <- rep(0, nrow(mygrid))
+for (i in 1:nrow(mygrid)) {
+    acc <- rep(0, k)
+    for (j in 1:k) {
+        kiana_train <- df_kianatrain[folds != j, ]
+        kiana_test <- df_kianatrain[folds == j, ]
+        mod <- rpart(Likability ~ .-URL-Title, df_kianatrain, 
+        control = list(minsplit = mygrid$minsplit[i], 
+            maxdepth = mygrid$maxdepth[i], cp = mygrid$cp[i]))
+        pred <- predict(mod, kiana_test, type = "class")
+        cm <- confusionMatrix(pred, kiana_test$Likability)
+        acc[j] <- cm$overall["Accuracy"]
+    }
+    mod_accuracy[i] <- mean(acc)
+    print(paste0("Iteration ", i, " out of ", nrow(mygrid), " completed."))
+    flush.console()
+}
+bst.mod <- which.max(mod_accuracy)
+mygrid[bst.mod, ]
+mod_accuracy[bst.mod]
+
+#ranger
+modranger <- ranger(Likability ~ .-URL-Title, df_kianatrain)
+modranger
+modranger$confusion.matrix
 #####################################################################################
 #Testing the model on the big set of all the movies:
